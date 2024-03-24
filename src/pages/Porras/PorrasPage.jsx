c69a64f27bb36c1d976b098c4c405b1ab2d4ad5f
Carlos Nauzet Hernández <carlos.hernandez@motork.io>
2024-03-24 15:51:23 +0100

    Pruebas formulario apuesta

--- src/pages/Porras/PorrasPage.jsx
+++ src/pages/Porras/PorrasPage.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import Button from "../../components/Button";
 import FormInput from "../../components/FormInput";
 
 const PorrasPage = () => {
+  const [Usuario, setUsuario] = useState("");
+  const [TextoLibre, setTextoLibre] = useState("");
+  const [Selector, setSelector] = useState("Apuestas");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const apuesta = { Usuario, TextoLibre, Selector };
+    console.log(apuesta);
+
+    // TODO: hacer el POST bien!
+
+    fetch("https://api.miporra.es/v1.0/newclubBet", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(apuesta),
+    }).then(() => {
+      console.log("Nueva apuesta añadida con éxito");
+    });
+  };
+
   return (
     <div>
       <h1>Ver porras</h1>
@@ -17,16 +38,49 @@ const PorrasPage = () => {
       <h1>Registro</h1>
       <FormInput
         type="text"
-        label="nombre de usuario"
-        defaultValue="Introduce tu nombre"
+        required
+        label="Nombre de usuario"
+        // Value={title}
+        // onChange={(e) => setTitle(e.target.value)}
         name="username"
       />
       <FormInput
         type="password"
-        label="password"
+        label="Password"
         defaultValue=""
         name="password"
       />
+
+      <div className="input-field">
+        <h2>PRUEBAS</h2>
+        <form className="input" onSubmit={handleSubmit}>
+          <label>Nombre de usuario</label>
+          <input
+            type="text"
+            required
+            value={Usuario}
+            onChange={(e) => setUsuario(e.target.value)}
+          />
+          <label>Texto libre</label>
+          <textarea
+            required
+            value={TextoLibre}
+            onChange={(e) => setTextoLibre(e.target.value)}
+          ></textarea>
+          <label>Selector</label>
+          <select
+            value={Selector}
+            onChange={(e) => setSelector(e.target.value)}
+          >
+            <option value="Apuestas">Apuestas</option>
+            <option value="Rifas">Rifas</option>
+          </select>
+          <button>Ver</button>
+          <p>{Usuario}</p>
+          <p>{TextoLibre}</p>
+          <p>{Selector}</p>
+        </form>
+      </div>
     </div>
   );
 };

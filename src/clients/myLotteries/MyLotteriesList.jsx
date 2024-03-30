632eac8021d7326eb71b82d2d89f97171c364e98
Fernando Jarilla <fjarilla@gmail.com>
2024-03-30 16:15:33 +0100

    Added more navigation to the clients application

--- src/clients/myLotteries/MyLotteriesList.jsx
+++ src/clients/myLotteries/MyLotteriesList.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom"
+import Button from "../../components/Button"
+
 const MyLotteriesList = () => {
 
     return (
         <div>
             <p>Listado de rifas</p>
+            <Link to="/myLotteryDetail">
+                <Button type="primary-cta">Rifa seleccionada</Button>
+            </Link>
+            <Link to="/myClubsList">
+                <Button type="primary-cta">Listado de porras</Button>
+            </Link>
         </div>
     )
 }

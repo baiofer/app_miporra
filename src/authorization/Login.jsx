6ee31cc9485c0dffe360c3fc3253af9da874b7f2
Fernando Jarilla <fjarilla@gmail.com>
2024-03-31 08:54:45 +0200

    Added basic login funcionality without style. Don't save the token

--- src/authorization/Login.jsx
+++ src/authorization/Login.jsx
@@ -1,22 +1,50 @@
-import { Button } from '@mui/material'
-import { Link } from 'react-router-dom'
+import Button from '../components/Button.jsx'
+import { useNavigate } from 'react-router-dom'
 import { useDispatch } from "react-redux"
 import { setOrigin } from "../redux/reducers/originReducer"
+import FormInput from '../components/FormInput.jsx'
+import { useState } from 'react'
+import { client } from '../api/config/client.js'
 
 const Login = () => {
 
-    const dispatch = useDispatch()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [token, setToken] = useState("")
 
-    const handleOnClick = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleOnClick = async () => {
+        console.log(email, password)
+        const token = await client.post('/login', { email, password })
+        setToken(token.data.token)
+        console.log('Token: ', token.data.token)
         dispatch(setOrigin('client'))
+        navigate('/myClubsList')
     }
 
     return (
         <div>
             <p>Página LOGIN</p>
-            <Link to="/myClubsList">
-                <Button type="primary-cta" onClick={handleOnClick}>Login correcto</Button>
-            </Link>
+            <FormInput 
+                type="text"
+                required
+                value={email}
+                onChange={ e => setEmail(e.target.value)}
+                label="Email"
+                name="email"
+            />
+            <FormInput 
+                type="password"
+                required
+                value={password}
+                onChange={ e => setPassword(e.target.value)}
+                label="Contraseña"
+                name="password"
+            />
+            <Button type="primary-cta" onClick={handleOnClick}>Acceso</Button>
+            <p>Token: { token }</p>
         </div>
     )
 }
--- src/clients/myClubs/MyClubsList.jsx
+++ src/clients/myClubs/MyClubsList.jsx
@@ -3,6 +3,9 @@ import Button from "../../components/Button"
 
 const MyClubsList = () => {
 
+    const handleClick = () => {
+        console.log('Click')
+    }
     return (
         <div>
             <p>Listado de porras del bar</p>
@@ -10,7 +13,7 @@ const MyClubsList = () => {
                 <Button type="primary-cta">Porra seleccionada</Button>
             </Link>
             <Link to="/myLotteriesList">
-                <Button type="primary-cta">Listado de rifas</Button>
+                <Button type="primary-cta" onClick={handleClick}>Listado de rifas</Button>
             </Link>
         </div>
     )

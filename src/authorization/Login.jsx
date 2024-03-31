0b23ccb88bdedec30d848f6b31ea9b26d8686a7e
Fernando Jarilla <fjarilla@gmail.com>
2024-03-31 09:08:35 +0200

    Added js-cookie library. Modified login to put the token in a secure cookie.

--- src/authorization/Login.jsx
+++ src/authorization/Login.jsx
@@ -5,6 +5,7 @@ import { setOrigin } from "../redux/reducers/originReducer"
 import FormInput from '../components/FormInput.jsx'
 import { useState } from 'react'
 import { client } from '../api/config/client.js'
+import Cookies from 'js-cookie'
 
 const Login = () => {
 
@@ -19,6 +20,7 @@ const Login = () => {
         console.log(email, password)
         const token = await client.post('/login', { email, password })
         setToken(token.data.token)
+        Cookies.set('token', token.data.token, { secure: true, sameSite: 'Strict' });
         console.log('Token: ', token.data.token)
         dispatch(setOrigin('client'))
         navigate('/myClubsList')

301d71bb42f7fe0296d53db6e5ef04c8407b1020
Fernando Jarilla <fjarilla@gmail.com>
2024-04-02 08:44:16 +0200

    Created Button.css

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import './Button.css'
 
 function Button({ variant, children, ...props }) {
   return <button className={variant} {...props} style={{...props}}>{children}</button>;

68964236501b43a2cdbf3a6075acce3c87c461b6
Fernando Jarilla <fjarilla@gmail.com>
2024-04-14 22:32:53 +0200

    Added delete token in logout

--- src/authorization/Logout.jsx
+++ src/authorization/Logout.jsx
@@ -10,10 +10,15 @@ const Logout = () => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
+    const deleteCookie = (name) => {
+        document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    };
+
     useEffect(() => {
         dispatch(setOrigin('user'));
         dispatch(setClientLogged({}))
         removeAuthorizationHeader()
+        deleteCookie('token')
         navigate('/porras')
     }, [dispatch, navigate]);
 

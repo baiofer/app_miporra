394b3c798f7286e29b615b6cd94031e55866a4ec
Fernando Jarilla <fjarilla@gmail.com>
2024-04-17 23:38:38 +0200

    Added menu icon to navBar

--- src/Usuarios/pages/Navigation/Navbar.jsx
+++ src/Usuarios/pages/Navigation/Navbar.jsx
@@ -1,23 +1,60 @@
 import { NavLink } from "react-router-dom";
 import logo from '../../../images/LOGO.png'
 import './Navbar.css'
+import menu from '../../../images/menu.png'
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
+
 
 function Navbar() {
+
+  const navigate = useNavigate()
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const handleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  const handleClubBets = ()=> {
+    setIsMenuOpen(false)
+    navigate("/clubs")
+  }
+
+  const handleLotteryBets = ()=> {
+    setIsMenuOpen(false)
+    navigate("/lottery-bets")
+  }
+
+  const handleClients = ()=> {
+    setIsMenuOpen(false)
+    navigate("/clients")
+  }
+
   return (
     <div className="navBar">
       <NavLink to="/homepage" className="navLink">
         <img className="logo" src={logo} alt="Logo" />
       </NavLink>
       <ul className="navBar-items">
-        <NavLink to="/clubs" className="navLink">
-          Apostar
-        </NavLink>
-        <NavLink to="/lottery-bets" className="navLink">
-          Rifas
-        </NavLink>
-        <NavLink to="/clients" className="navLink">
-          Encuentra tu bar
-        </NavLink>
+        <button className="navLink" onClick={handleMenu} >
+          <img src={menu} alt="Icono de menú" className="navBar-menu" />
+        </button>
+        {
+          isMenuOpen && (
+            <div className="menu-container">
+              <button onClick={handleClubBets} className="menu-item">
+                Apostar
+              </button>
+              <button onClick={handleLotteryBets} className="menu-item">
+                Rifas
+              </button>
+              <button onClick={handleClients} className="menu-item">
+                Encuentra tu bar
+              </button>
+            </div>
+          )
+        }  
         <NavLink to="/login" className="navLink">
           <span className="navLoginContainer">
             Soy un bar
